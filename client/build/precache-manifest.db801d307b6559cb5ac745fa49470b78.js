d8ee9384a6a0fa7307ebb2dd18a2ff6e8fa12738
David <david.bland@hotmail.com>
2019-11-27 14:00:05 -0500

    adding build folder for heroku

---
+++ client/build/precache-manifest.db801d307b6559cb5ac745fa49470b78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ba9887298f54d6c8c857dfadc1bad2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e4e102a612f9717829f",
+    "url": "/static/css/main.eb815cce.chunk.css"
+  },
+  {
+    "revision": "a993d48fe84cbf8dd4df",
+    "url": "/static/js/2.67dc2b62.chunk.js"
+  },
+  {
+    "revision": "9e4e102a612f9717829f",
+    "url": "/static/js/main.5d8e2de9.chunk.js"
+  },
+  {
+    "revision": "857ee18269a7f73a12ff",
+    "url": "/static/js/runtime-main.0191581f.js"
+  },
+  {
+    "revision": "62af69d8d1c72dc8f331d09a8e292fad",
+    "url": "/static/media/allianceFlag.62af69d8.jpg"
+  },
+  {
+    "revision": "b1a9bc25e19da86894a0d225e874caa7",
+    "url": "/static/media/ironForge.b1a9bc25.jpg"
+  },
+  {
+    "revision": "7ea6147919df0380a3815a916d0f761c",
+    "url": "/static/media/wowIcon.7ea61479.png"
+  }
+]);
