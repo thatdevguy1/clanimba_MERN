d2e4321378657e271cee43e0cc628e1a9cfa6e0b
David <david.bland@hotmail.com>
2019-11-27 13:49:48 -0500

    changed server path for /

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 /* ROUTES */
